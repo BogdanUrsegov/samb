07af593b16d94e2c676c105d45871bc96e4946ba
Богдан <bogdanursegov0203@gmail.com>
2026-07-01 05:25:57 +0500

    fix

--- bot/admin/handlers/commands.py
+++ bot/admin/handlers/commands.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command
 from bot.admin.filters import IsAdmin
+from bot.create_bot import ADMIN_ID
 from bot.admin.keyboards import admin_menu_keyboard
 
 router = Router()
@@ -9,4 +10,7 @@ router.message.filter(Command("admin_menu"), IsAdmin())
 
 @router.message(Command("admin_menu"))
 async def admin_menu(message: Message):
+    if message.from_user.id != int(ADMIN_ID):  # Замените на реальные ID администраторов
+        await message.answer("❌ У вас нет доступа к админ-панели.")
+        return
     await message.answer("👨‍💼 <b>Админ-панель</b>", reply_markup=admin_menu_keyboard())
