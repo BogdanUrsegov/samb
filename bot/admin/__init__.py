52f67bb8d1453fc8389a8b757eceb3f55f0a8bd5
BogdanUrsegov <102915294+BogdanUrsegov@users.noreply.github.com>
2026-08-16 02:08:07 +0500

    Register administrator management router

--- bot/admin/__init__.py
+++ bot/admin/__init__.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from bot.admin.handlers import callbacks, commands, fsm, referrals
+from bot.admin.handlers import callbacks, commands, fsm, referrals, admins
 from bot.admin.filters import IsAdmin
 
 # Создаем главный роутер админки
@@ -10,6 +10,7 @@ admin_router.message.filter(IsAdmin())
 admin_router.callback_query.filter(IsAdmin())
 
 # Подключаем все роутеры админки
+admin_router.include_router(admins.router)
 admin_router.include_router(callbacks.router)
 admin_router.include_router(commands.router)
 admin_router.include_router(fsm.router)
