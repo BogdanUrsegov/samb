edc88beac62ae5d1c62621244a5df2c6f8c7ab68
BogdanUrsegov <102915294+BogdanUrsegov@users.noreply.github.com>
2026-08-16 03:40:15 +0500

    refactor(admin): simplify router structure

--- bot/admin/__init__.py
+++ bot/admin/__init__.py
@@ -1,17 +1,11 @@
 from aiogram import Router
-from bot.admin.handlers import callbacks, commands, fsm, referrals, admins
+
 from bot.admin.filters import IsAdmin
+from bot.admin.handlers.admin import router as admin_handlers_router
+from bot.admin.handlers.admin import superadmin_router
 
-# Создаем главный роутер админки
 admin_router = Router()
-
-# Все handlers админки доступны только пользователям из таблицы admins.
 admin_router.message.filter(IsAdmin())
 admin_router.callback_query.filter(IsAdmin())
-
-# Подключаем все роутеры админки
-admin_router.include_router(admins.router)
-admin_router.include_router(callbacks.router)
-admin_router.include_router(commands.router)
-admin_router.include_router(fsm.router)
-admin_router.include_router(referrals.router)
+admin_router.include_router(admin_handlers_router)
+admin_router.include_router(superadmin_router)
