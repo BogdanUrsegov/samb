27ebf90fad070e1f4247c9ee270f6f3b1fc7c550
root <root@vm-002910e1.na4u.ru>
2026-08-17 14:23:50 +0000

    fix: update advertisement display

--- bot/utils/show_advert.py
+++ bot/utils/show_advert.py
@@ -13,7 +13,7 @@ async def show_advert(user_id):
             async with session.post(
                 "https://api.gramads.net/ad/SendPost",
                 headers={
-                    f"Authorization": "Bearer {GRAMADS_API_KEY}",
+                    f"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNTA2OCIsImp0aSI6ImJkODAxYjE0LWE4ZTUtNDg1MC1hNzNjLTQyN2M5ODhjNzc1YiIsIm5hbWUiOiLQkNC90L7QvdC40LzQvdGL0LUg0YHQvtC-0LHRidC10L3QuNGPIiwiYm90aWQiOiIyMTY4OSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiMjUwNjgiLCJuYmYiOjE3ODY5MjYxNDYsImV4cCI6MTc4NzEzNDk0NiwiaXNzIjoiU3R1Z25vdiIsImF1ZCI6IlVzZXJzIn0.PWTyLqVE2hoioVAxCBLZM9Mu-dlALMf1sTuoRCO_n4A",
                     "Content-Type": "application/json",
                 },
                 json={"SendToChatId": user_id},
