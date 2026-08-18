5d543b14043532004c8dc0644f19c1f4161a0c4a
BogdanUrsegov <102915294+BogdanUrsegov@users.noreply.github.com>
2026-08-18 14:22:41 +0500

    feat: register referral deletion handler

--- bot/admin/__init__.py
+++ bot/admin/__init__.py
@@ -2,7 +2,9 @@ from aiogram import Router
 
 from bot.admin.handlers.admin import router as admin_handlers_router
 from bot.admin.handlers.admin import superadmin_router
+from bot.admin.handlers.referral_delete import router as referral_delete_router
 
 admin_router = Router()
 admin_router.include_router(admin_handlers_router)
 admin_router.include_router(superadmin_router)
+admin_router.include_router(referral_delete_router)
